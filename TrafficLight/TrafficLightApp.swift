1aa07873f2e6a8d747494e7a80896cb3807bd77b
AlenaBelenets <alenaharenkova@gmail.com>
2022-09-09 13:32:34 +0300

    Initial Commit

---
+++ TrafficLight/TrafficLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightApp.swift
+//  TrafficLight
+//
+//  Created by Alena Belenets on 09.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
